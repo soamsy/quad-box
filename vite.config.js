a54e950a4d9c90ecfba5ac1f74ad56b1e7eda629
Soames <thsoams@gmail.com>
2025-05-28 19:30:23 -0700

    Update base path via github actions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import tailwindcss from '@tailwindcss/vite'
+import process from 'process'
+
 export default defineConfig({
   plugins: [
     tailwindcss(),
     svelte(),
   ],
-  base: '',
+  base: process.env.BASE_PATH || '/',
 })
