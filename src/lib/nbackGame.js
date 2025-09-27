a619af7e7832a1d8cfb6f701ef766181d74e45e2
Soames <thsoams@gmail.com>
2025-09-26 18:22:47 -0600

    Tweak probabilities of variable mode

--- src/lib/nbackGame.js
+++ src/lib/nbackGame.js
@@ -90,7 +90,7 @@ export class NBackGame {
 
   generateNSequence() {
     if (this.rules === 'variable') {
-      return new Array(this.numTrials).fill().map(() => Math.floor(Math.pow(this.random(), 0.5) * this.nBack) + 1)
+      return new Array(this.numTrials).fill().map(() => Math.floor(Math.pow(this.random(), this.random() < 0.5 ? 0.25 : 0.6) * this.nBack) + 1)
     } else {
       return new Array(this.numTrials).fill(this.nBack)
     }
